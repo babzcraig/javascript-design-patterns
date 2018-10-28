8c2bab1472a0b4ccf1b3f62b9db9eb6a3b420b26
Babs Craig <babscraig@gmail.com>
2018-10-28 12:30:47 +0100

    add old pubsub code

---
+++ pubsub-old/index.js
@@ -0,0 +1,7 @@
+const moduleA = require("./moduleA");
+const moduleB = require("./moduleB");
+
+// We use moduleB's publishEvent() method
+moduleB.publishEvent();
+moduleB.publishEvent();
+moduleB.publishEvent();
