174e03684af3d815f285bbe19e981936c6b3b6a1
Babs Craig <babscraig@gmail.com>
2018-11-15 15:51:26 +0100

    remove old pubsub code

--- pubsub-old/index.js
+++
@@ -1,7 +0,0 @@
-const moduleA = require("./moduleA");
-const moduleB = require("./moduleB");
-
-// We use moduleB's publishEvent() method
-moduleB.publishEvent();
-moduleB.publishEvent();
-moduleB.publishEvent();
--- pubsub-old/moduleA.js
+++
@@ -1,8 +0,0 @@
-const pubSub = require("./pub-sub");
-
-let subscription;
-
-subscription = pubSub.subscribe("atopic", function(data) {
-  console.log("atopic was published with data: ", data);
-  subscription.unsubscribe();
-});
